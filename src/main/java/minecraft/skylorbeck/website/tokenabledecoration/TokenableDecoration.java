71867cfe75a34cb809972ad0a968209238aeea42
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-08-11 12:10:36 -0700

    Initial commit

---
+++ src/main/java/minecraft/skylorbeck/website/tokenabledecoration/TokenableDecoration.java
@@ -0,0 +1,10 @@
+package minecraft.skylorbeck.website.tokenabledecoration;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TokenableDecoration implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/minecraft/skylorbeck/website/tokenabledecoration/client/TokenableDecorationClient.java
@@ -0,0 +1,12 @@
+package minecraft.skylorbeck.website.tokenabledecoration.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+
+@Environment(net.fabricmc.api.EnvType.CLIENT)
+public class TokenableDecorationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
